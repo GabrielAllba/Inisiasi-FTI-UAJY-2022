bea1fbf4acbdb7b6a66580c89918c0d29d3368c2
rielgbrl <rielgasy9@gmail.com>
2023-06-11 01:17:28 +0700

    add factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Admin::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Admin::factory()->create([
+            'name' => 'admin',
+            'email' => 'inisiasiftiuajy@gmail.com',
+            'email_verified_at' => NULL,
+            'password'=> Hash::make('Ftiuajyinisiasi2022@)@@'),
+            'profile_photo_path'=>NULL,
+            'remember_token'=>NULL,
+            'created_at'=>NULL,
+            'updated_at'=>NULL
+        ]);
     }
 }
